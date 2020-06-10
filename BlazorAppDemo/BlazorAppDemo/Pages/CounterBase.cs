85f84aaa0a369358b169ee9f7fa12bc8f690156e
Sunil Singh A <sunil.singh.a@sap.com>
2020-06-10 12:31:34 +0530

    new clone

---
+++ BlazorAppDemo/BlazorAppDemo/Pages/CounterBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorAppDemo.Pages
+{
+    public class CounterBase : ComponentBase
+    {
+        protected int currentCount = 0;
+
+        protected void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+
+}
